970b1da2ba16544ded3d7cf0e6145fbd23d6508e
soheilrahsaz <soheilrahsaz.sr@gmail.com>
2023-10-26 22:28:24 +0330

    bug with registerUserDto having no default constructor

--- src/main/java/com/moses33/habitof6/web/dto/RegisterUserDto.java
+++ src/main/java/com/moses33/habitof6/web/dto/RegisterUserDto.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterUserDto {
     @Length(max = 32)
     @NotEmpty
