221ab815d5d74684d204733e9b3f0a2889f642e4
soheil rahsaz <soheilrahsaz.sr@gmail.com>
2023-11-03 17:32:54 +0330

    removed HasAccessToHabit annotation
    didn't find any way to make it work with dynamic hasAuthority(...)

--- src/main/java/com/moses33/habitof6/web/controller/DayDoneController.java
+++ src/main/java/com/moses33/habitof6/web/controller/DayDoneController.java
@@ -1,13 +1,13 @@
 package com.moses33.habitof6.web.controller;
 
 import com.moses33.habitof6.service.DayDoneService;
-import com.moses33.habitof6.web.auth.HasAccessToHabit;
 import com.moses33.habitof6.web.dto.daydone.DayDoneDto;
 import com.moses33.habitof6.web.response.BaseResponse;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,9 +17,10 @@ public class DayDoneController extends BaseController{
     private final DayDoneService dayDoneService;
 
     @GetMapping
-    @HasAccessToHabit
+    @PreAuthorize("@habitAuthenticationManager.hasAccessToHabit(authentication, #habitId) && hasAuthority('dayDone.read')")
     public BaseResponse<Page<DayDoneDto>> getDayDones(@PathVariable @NotNull Integer habitId, @RequestParam(required = false) Integer pageNumber,
                                                       @RequestParam(required = false) Integer pageSize) {
         return new BaseResponse<>(dayDoneService.getDayDones(habitId, getPageRequest(pageNumber, pageSize)), HttpStatus.OK);
     }
+
 }
