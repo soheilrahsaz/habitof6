2ce74a9928f821b7a7f11f361605dc7cea54a66e
Soheil Rahsaz <soheilrahsaz.sr@gmail.com>
2023-11-15 11:46:05 +0330

    war packaing and change in sql user

--- src/main/java/com/moses33/habitof6/Habitof6Application.java
+++ src/main/java/com/moses33/habitof6/Habitof6Application.java
@@ -2,9 +2,10 @@ package com.moses33.habitof6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Habitof6Application {
+public class Habitof6Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Habitof6Application.class, args);
